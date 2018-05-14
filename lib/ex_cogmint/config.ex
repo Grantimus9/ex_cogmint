81f088cebe20f67ba825f45a16e7da0079b56ed8
Grant Nelson <grantmnelsn@gmail.com>
2018-05-13 21:33:30 -0400

    removed call to Mix in code as it's not available at runtime always

--- lib/ex_cogmint/config.ex
+++ lib/ex_cogmint/config.ex
@@ -62,21 +62,13 @@ defmodule ExCogmint.Config do
   end
 
   def get_server_url() do
-    case Mix.env() do
-      :dev ->
-        case Application.get_env(:ex_cogmint, :use_live) do
-          nil ->
-            "http://localhost:4000"
-          true ->
-            "https://cogmint-demo.herokuapp.com"
-          false ->
-            "https://cogmint-demo.herokuapp.com"
-        end
-      :prod ->
-        "https://cogmint-demo.herokuapp.com"
-
-      _ ->
+    case Application.get_env(:ex_cogmint, :use_live) do
+      nil ->
         "http://localhost:4000"
+      true ->
+        "https://cogmint-demo.herokuapp.com"
+      false ->
+        "https://cogmint-demo.herokuapp.com"
     end
   end
 end
