f9bdee2827f7b858c110004b832a174ae46bfdb5
Grant Nelson <grantmnelsn@gmail.com>
2020-04-02 23:53:46 -0400

    fixed endpoint

--- lib/ex_cogmint/worktask.ex
+++ lib/ex_cogmint/worktask.ex
@@ -7,7 +7,7 @@ defmodule ExCogmint.Worktask do
   # "localhost:4000/api/v1/projects/add_task"
   def build_get_task_request(uuid) when is_binary(uuid) do
     %{
-      path: "/api/v1/worktasks/" <> uuid,
+      path: "/api/v1/tasks/" <> uuid,
       method: :get
     }
   end
